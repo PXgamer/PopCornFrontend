b5a3f251896a16d7c70b8d5f578e357c1039c1e6
Konstantin Schmidt <assimilationstheorie@gmail.com>
2016-11-30 16:32:14 +0100

    img thumb

--- application/views/main.php
+++ application/views/main.php
@@ -22,7 +22,7 @@
 			foreach($movies as $movie) {
 		?>
 		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 movieModule">
-			<img src="<?php echo $movie->image_path ?>" alt="<?php echo $movie->name; ?>" class="img-responsive">
+			<img src="<?php echo $movie->image_path ?>" alt="<?php echo $movie->name; ?>" class="img-responsive img-thumbnail">
 			<h4><?php echo $movie->name; ?></h4>
 			<p><a href="/PopCornFrontend/movies/<?php echo $movie->id; ?>"><h5>Zur Detailansicht</h5></a></p>
 		</div>
